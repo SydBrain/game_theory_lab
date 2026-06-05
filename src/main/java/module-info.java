d2f86bae9ee4defe9fc713ae99a5051c6d9f1bc3
Pio Musco <piomusco99@gmail.com>
2026-06-05 14:16:03 +0200

    Fixed build

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module game.theory.lab {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens org.lab to javafx.graphics;
     opens org.lab.view to javafx.fxml, javafx.graphics;
-    opens org.lab to javafx.fxml, javafx.graphics;
+    opens org.lab.controller to javafx.fxml;
 }
